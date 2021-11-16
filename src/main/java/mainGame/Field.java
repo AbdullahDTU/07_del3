07e1a4314b1bcd462ba9ef200bf37606a8c29f55
Abdu <Abdu0901@ltkskole.dk>
2021-11-16 13:40:53 +0100

    Working on Fields Added Brown and Cyan fields

--- src/main/java/mainGame/Field.java
+++ src/main/java/mainGame/Field.java
@@ -8,12 +8,18 @@ import java.awt.*;
 
 public class Field {
 
-    private static final GUI_Field field1 = new GUI_Start(FieldConstants.START_NAME, FieldConstants.START_SUBTEXT, FieldConstants.START_DESCRIPTION, Color.RED, Color.WHITE);
-    //private static final GUI_Field field2 = new GUI_Street(FieldConstants.BURGER_NAME, FieldConstants.BURGER_SUBTEXT, FieldConstants.BURGER_DESCRIPTION, FieldConstants.BURGER_RENT, Color.BLUE, Color.WHITE);
+    private static final GUI_Field field1 = new GUI_Start(FieldConstants.START_NAME, FieldConstants.START_SUBTEXT, FieldConstants.START_DESCRIPTION, new Color(255, 0, 0), Color.WHITE);
+    private static final GUI_Field field2 = new GUI_Street(FieldConstants.BURGER_NAME, FieldConstants.BURGER_SUBTEXT, FieldConstants.BURGER_DESCRIPTION, FieldConstants.BURGER_RENT, new Color(140, 43, 5), Color.WHITE);
+    private static final GUI_Field field3 = new GUI_Street(FieldConstants.PIZZA_NAME, FieldConstants.PIZZA_SUBTEXT, FieldConstants.PIZZA_DESCRIPTION, FieldConstants.PIZZA_RENT, new Color(140, 43, 5), Color.WHITE);
+    private static final GUI_Field field4 = new GUI_Street(FieldConstants.SWEET_NAME, FieldConstants.SWEET_SUBTEXT, FieldConstants.SWEET_DESCRIPTION, FieldConstants.SWEET_RENT, new Color(0, 197, 255), Color.WHITE);
+    private static final GUI_Field field5 = new GUI_Street(FieldConstants.ICECREAM_NAME, FieldConstants.ICECREAM_SUBTEXT, FieldConstants.ICECREAM_DESCRIPTION, FieldConstants.ICECREAM_RENT, new Color(0, 197, 255), Color.WHITE);
 
     private static GUI_Field[] fields = new GUI_Field[]{
             field1,
-
+            field2,
+            field3,
+            field4,
+            field5
     };
 
 
--- src/main/java/mainGame/FieldConstants.java
+++ src/main/java/mainGame/FieldConstants.java
@@ -4,16 +4,29 @@ public class FieldConstants {
     //Title of field
     public static final String START_NAME = "Start";
     public static final String BURGER_NAME = "Burger Bar";
+    public static final String PIZZA_NAME = "Pizza Huset";
+    public static final String SWEET_NAME = "SWEETS SHOP";
+    public static final String ICECREAM_NAME = "ICECREAM SHOP";
 
     //Description of field
-    public static final String START_DESCRIPTION = "Når du passerer start få 1M";
-    public static final String BURGER_DESCRIPTION = "Dette felt koster 1M";
+    public static final String START_DESCRIPTION = "Recieve 1M when passing or landing here";
+    public static final String BURGER_DESCRIPTION = "Buy this field for 1M";
+    public static final String PIZZA_DESCRIPTION = "Buy this field for 1M";
+    public static final String SWEET_DESCRIPTION = "Buy this field for 1M";
+    public static final String ICECREAM_DESCRIPTION = "Buy this field for 1M";
+
+    //Rent of field
+    public static final String BURGER_RENT = "1";
+    public static final String PIZZA_RENT = "1";
+    public static final String SWEET_RENT = "1";
+    public static final String ICECREAM_RENT = "1";
 
     //Subtext of field
     public static final String START_SUBTEXT = "Start field";
-    public static final String BURGER_SUBTEXT = "Burger Bar";
+    public static final String BURGER_SUBTEXT = BURGER_RENT + "M";
+    public static final String PIZZA_SUBTEXT = PIZZA_RENT + "M";
+    public static final String SWEET_SUBTEXT = SWEET_RENT + "M";
+    public static final String ICECREAM_SUBTEXT = ICECREAM_RENT + "M";
 
-    //Rent of field
-    public static final int BURGER_RENT = 1;
 
 }
