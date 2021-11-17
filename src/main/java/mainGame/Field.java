05d5496718e266b0c124d69af9aa27537d4affa9
Abdu <Abdu0901@ltkskole.dk>
2021-11-17 13:59:29 +0100

    Added Half of the fields to the board

--- src/main/java/mainGame/Field.java
+++ src/main/java/mainGame/Field.java
@@ -1,30 +1,43 @@
 package mainGame;
 
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Start;
-import gui_fields.GUI_Street;
+import gui_fields.*;
 
 import java.awt.*;
 
 public class Field {
-
+    //Constructing fields of type Street, Chance, Jail and Parking
     private static final GUI_Field field1 = new GUI_Start(FieldConstants.START_NAME, FieldConstants.START_SUBTEXT, FieldConstants.START_DESCRIPTION, new Color(255, 0, 0), Color.WHITE);
     private static final GUI_Field field2 = new GUI_Street(FieldConstants.BURGER_NAME, FieldConstants.BURGER_SUBTEXT, FieldConstants.BURGER_DESCRIPTION, FieldConstants.BURGER_RENT, new Color(140, 43, 5), Color.WHITE);
     private static final GUI_Field field3 = new GUI_Street(FieldConstants.PIZZA_NAME, FieldConstants.PIZZA_SUBTEXT, FieldConstants.PIZZA_DESCRIPTION, FieldConstants.PIZZA_RENT, new Color(140, 43, 5), Color.WHITE);
-    private static final GUI_Field field4 = new GUI_Street(FieldConstants.SWEET_NAME, FieldConstants.SWEET_SUBTEXT, FieldConstants.SWEET_DESCRIPTION, FieldConstants.SWEET_RENT, new Color(0, 197, 255), Color.WHITE);
-    private static final GUI_Field field5 = new GUI_Street(FieldConstants.ICECREAM_NAME, FieldConstants.ICECREAM_SUBTEXT, FieldConstants.ICECREAM_DESCRIPTION, FieldConstants.ICECREAM_RENT, new Color(0, 197, 255), Color.WHITE);
+    private static final GUI_Field field4 = new GUI_Chance(FieldConstants.CHANCE_NAME, FieldConstants.CHANCE_SUBTEXT, FieldConstants.CHANCE_DESCRIPTION, new Color(255, 220, 38), Color.WHITE);
+    private static final GUI_Field field5 = new GUI_Street(FieldConstants.SWEET_NAME, FieldConstants.SWEET_SUBTEXT, FieldConstants.SWEET_DESCRIPTION, FieldConstants.SWEET_RENT, new Color(0, 197, 255), Color.WHITE);
+    private static final GUI_Field field6 = new GUI_Street(FieldConstants.ICECREAM_NAME, FieldConstants.ICECREAM_SUBTEXT, FieldConstants.ICECREAM_DESCRIPTION, FieldConstants.ICECREAM_RENT, new Color(0, 197, 255), Color.WHITE);
+    private static final GUI_Field field7 = new GUI_Jail("Jail.jpg", FieldConstants.JAIL_NAME, FieldConstants.JAIL_SUBTEXT, FieldConstants.JAIL_DESCRIPTION, new Color(125, 125, 125), Color.WHITE);
+    private static final GUI_Field field8 = new GUI_Street(FieldConstants.MUSEUM_NAME, FieldConstants.MUSEUM_SUBTEXT, FieldConstants.MUSEUM_DESCRIPTION, FieldConstants.MUSEUM_RENT, new Color(151, 24, 255), Color.WHITE);
+    private static final GUI_Field field9 = new GUI_Street(FieldConstants.LIBRARY_NAME, FieldConstants.LIBRARY_SUBTEXT, FieldConstants.LIBRARY_DESCRIPTION, FieldConstants.LIBRARY_RENT, new Color(151, 24, 255), Color.WHITE);
+    private static final GUI_Field field10 = new GUI_Chance(FieldConstants.CHANCE_NAME, FieldConstants.CHANCE_SUBTEXT, FieldConstants.CHANCE_DESCRIPTION, new Color(255, 220, 38), Color.WHITE);
+    private static final GUI_Field field11 = new GUI_Street(FieldConstants.SKATER_NAME, FieldConstants.SKATER_SUBTEXT, FieldConstants.SKATER_DESCRIPTION, FieldConstants.SKATER_RENT, new Color(255, 184, 0), Color.WHITE);
+    private static final GUI_Field field12 = new GUI_Street(FieldConstants.POOL_NAME, FieldConstants.POOL_SUBTEXT, FieldConstants.POOL_DESCRIPTION, FieldConstants.POOL_RENT, new Color(255, 184, 0), Color.WHITE);
+    private static final GUI_Field field13 = new GUI_Tax(FieldConstants.PARKING_NAME,FieldConstants.PARKING_SUBTEXT,FieldConstants.PARKING_DESCRIPTION,new Color(255, 255, 255), Color.BLACK);
 
+    //GUI Field Array. The fields are shown in the order they are added in
     private static GUI_Field[] fields = new GUI_Field[]{
             field1,
             field2,
             field3,
             field4,
-            field5
+            field5,
+            field6,
+            field7,
+            field8,
+            field9,
+            field10,
+            field11,
+            field12,
+            field13
     };
-
 
     public static GUI_Field[] getFields() {
         return fields;
     }
-
 }
--- src/main/java/mainGame/FieldConstants.java
+++ src/main/java/mainGame/FieldConstants.java
@@ -1,32 +1,59 @@
 package mainGame;
 
+//Easy editable Variables containing the Title, Rent, Description and Subtext
 public class FieldConstants {
     //Title of field
-    public static final String START_NAME = "Start";
-    public static final String BURGER_NAME = "Burger Bar";
-    public static final String PIZZA_NAME = "Pizza Huset";
+    public static final String START_NAME = "START";
+    public static final String CHANCE_NAME = "?";
+    public static final String JAIL_NAME = "JAIL";
+    public static final String PARKING_NAME = "FREE PARKING";
+    public static final String BURGER_NAME = "BURGER HOUSE";
+    public static final String PIZZA_NAME = "PIZZA HOUSE";
     public static final String SWEET_NAME = "SWEETS SHOP";
     public static final String ICECREAM_NAME = "ICECREAM SHOP";
+    public static final String MUSEUM_NAME = "MUSEUM";
+    public static final String LIBRARY_NAME = "LIBRARY";
+    public static final String SKATER_NAME = "SKATER PARK";
+    public static final String POOL_NAME = "SWIMMING POOL";
 
-    //Description of field
-    public static final String START_DESCRIPTION = "Recieve 1M when passing or landing here";
-    public static final String BURGER_DESCRIPTION = "Buy this field for 1M";
-    public static final String PIZZA_DESCRIPTION = "Buy this field for 1M";
-    public static final String SWEET_DESCRIPTION = "Buy this field for 1M";
-    public static final String ICECREAM_DESCRIPTION = "Buy this field for 1M";
 
     //Rent of field
     public static final String BURGER_RENT = "1";
     public static final String PIZZA_RENT = "1";
     public static final String SWEET_RENT = "1";
     public static final String ICECREAM_RENT = "1";
+    public static final String MUSEUM_RENT = "2";
+    public static final String LIBRARY_RENT = "2";
+    public static final String SKATER_RENT = "2";
+    public static final String POOL_RENT = "2";
+
+    //Description of field
+    public static final String START_DESCRIPTION = "Recieve 1M when passing or landing here";
+    public static final String CHANCE_DESCRIPTION = "Draw a Chance card";
+    public static final String JAIL_DESCRIPTION = "You are now in Jail";
+    public static final String PARKING_DESCRIPTION = "You are parking for free";
+    public static final String BURGER_DESCRIPTION = String.format("Buy this field for %S M", BURGER_RENT);
+    public static final String PIZZA_DESCRIPTION = String.format("Buy this field for %S M", PIZZA_RENT);
+    public static final String SWEET_DESCRIPTION = String.format("Buy this field for %S M", SWEET_RENT);
+    public static final String ICECREAM_DESCRIPTION = String.format("Buy this field for %S M", ICECREAM_RENT);
+    public static final String MUSEUM_DESCRIPTION = String.format("Buy this field for %S M", MUSEUM_RENT);
+    public static final String LIBRARY_DESCRIPTION = String.format("Buy this field for %S M", LIBRARY_RENT);
+    public static final String SKATER_DESCRIPTION = String.format("Buy this field for %S M", SKATER_RENT);
+    public static final String POOL_DESCRIPTION = String.format("Buy this field for %S M", POOL_RENT);
 
     //Subtext of field
-    public static final String START_SUBTEXT = "Start field";
+    public static final String START_SUBTEXT = "";
+    public static final String CHANCE_SUBTEXT = "CHANCE";
+    public static final String JAIL_SUBTEXT = "JAIL";
+    public static final String PARKING_SUBTEXT = "";
     public static final String BURGER_SUBTEXT = BURGER_RENT + "M";
     public static final String PIZZA_SUBTEXT = PIZZA_RENT + "M";
     public static final String SWEET_SUBTEXT = SWEET_RENT + "M";
     public static final String ICECREAM_SUBTEXT = ICECREAM_RENT + "M";
+    public static final String MUSEUM_SUBTEXT = MUSEUM_RENT + "M";
+    public static final String LIBRARY_SUBTEXT = LIBRARY_RENT + "M";
+    public static final String SKATER_SUBTEXT = SKATER_RENT + "M";
+    public static final String POOL_SUBTEXT = POOL_RENT + "M";
 
 
 }
