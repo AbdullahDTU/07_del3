fd2d64419ddd277e7caf4f62455262f1b77b6ec0
Betim <s216250@student.dtu.dk>
2021-11-24 20:02:45 +0100

    Add unit test for start field test

--- src/test/java/mainGame/FieldTest.java
+++ src/test/java/mainGame/FieldTest.java
@@ -1,5 +1,6 @@
 package mainGame;
 
+import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,12 @@ public class FieldTest {
         //Test number of fields is exact 24
         assertEquals(24, Field.getFields().length);
 
+    }
+
+    @Test
+    public void testFirstFieldIsStartField() {
+        GUI_Field[] fields = Field.getFields();
+        assertEquals(FieldConstants.START_NAME ,fields[0].getTitle());
     }
 
 }
